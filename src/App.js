feb8532f60b4de969db6d0d316edc46cb1c85784
Suyash00 <suyashhandy@gmail.com>
2024-01-05 22:11:17 +0530

    changed error handling and used then, catch and finally

--- src/App.js
+++ src/App.js
@@ -38,26 +38,49 @@ const WeatherDisplay = ({ city }) => {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const getWeatherData = async () => {
-    try {
-      const res = await axios.get(
-        `https://api.weatherapi.com/v1/current.json?key=315c9b1f8cb943f1a1c50245240401&q=${city}`
-      );
+  // const getWeatherData = async () => {
+  //   try {
+  //     const res = await axios.get(
+  //       `https://api.weatherapi.com/v1/current.json?key=315c9b1f8cb943f1a1c50245240401&q=${city}`
+  //     );
 
-      // const res = await data.json();
-      setWeatherData(res.data);
-    } catch (error) {
-      console.error("Error fetching data: ", error);
-      alert("Failed to fetch weather data");
-    } finally {
-      setLoading(false);
-    }
-  };
+  //     // const res = await data.json();
+  //     setWeatherData(res.data);
+  //   } catch (error) {
+  //     console.error("Error fetching data: ", error);
+  //     alert("Failed to fetch weather data");
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (city) {
+  //     setLoading(true);
+  //     getWeatherData();
+  //   }
+  // }, [city]);
 
   useEffect(() => {
     if (city) {
       setLoading(true);
-      getWeatherData();
+      axios
+        .get(`https://api.weatherapi.com/v1/current.json`, {
+          params: {
+            key: "315c9b1f8cb943f1a1c50245240401",
+            q: city,
+          },
+        })
+        .then((response) => {
+          setWeatherData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+          alert("Failed to fetch weather data");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
     }
   }, [city]);
 
