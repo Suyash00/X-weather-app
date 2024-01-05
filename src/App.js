8d3952b8d06ce4f0936e2e222b9a424c46c015a9
Suyash00 <suyashhandy@gmail.com>
2024-01-05 22:28:41 +0530

    weather changed whole code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 const SearchBar = ({ onSearch }) => {
   const [city, setCity] = useState("");
+
   const handleSearch = () => {
     onSearch(city);
-  };
-
-  const changeHandler = (e) => {
-    setCity(e.target.value);
   };
 
   return (
@@ -17,7 +14,7 @@ const SearchBar = ({ onSearch }) => {
       <input
         type="text"
         value={city}
-        onChange={changeHandler}
+        onChange={(e) => setCity(e.target.value)}
         placeholder="Enter city name"
       />
       <button onClick={handleSearch}>Search</button>
@@ -37,29 +34,6 @@ const WeatherCard = ({ title, data }) => {
 const WeatherDisplay = ({ city }) => {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  // const getWeatherData = async () => {
-  //   try {
-  //     const res = await axios.get(
-  //       `https://api.weatherapi.com/v1/current.json?key=315c9b1f8cb943f1a1c50245240401&q=${city}`
-  //     );
-
-  //     // const res = await data.json();
-  //     setWeatherData(res.data);
-  //   } catch (error) {
-  //     console.error("Error fetching data: ", error);
-  //     alert("Failed to fetch weather data");
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (city) {
-  //     setLoading(true);
-  //     getWeatherData();
-  //   }
-  // }, [city]);
 
   useEffect(() => {
     if (city) {
@@ -99,7 +73,7 @@ const WeatherDisplay = ({ city }) => {
           />
           <WeatherCard
             title="Condition"
-            data={`${weatherData.current.condition.text}`}
+            data={weatherData.current.condition.text}
           />
           <WeatherCard
             title="Wind Speed"
@@ -114,8 +88,8 @@ const WeatherDisplay = ({ city }) => {
 function App() {
   const [city, setCity] = useState("");
 
-  const handleSearch = (searchedVal) => {
-    setCity(searchedVal);
+  const handleSearch = (searchCity) => {
+    setCity(searchCity);
   };
 
   return (
